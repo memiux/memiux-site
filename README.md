8eaad1eca4a47414eb076317bf3825136d80fa9a
memiux <me@memiux.com>
2021-08-22 09:14:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+I wanna have fun and chat with someone  b e · s i d e s   m y · s e l f !
+
+<sub>...while I wait, I will stay happy! 😊</sub>
+
+<a href="https://github.com/memiux"><img src="https://user-images.githubusercontent.com/37290/124840841-7bf60d80-df51-11eb-8abb-f69434544136.png"></a>
---
+++ main.js
@@ -0,0 +1,15 @@
+addEventListener('fetch', event => {
+	event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+	let response = await fetch(request);
+	response = new Response(response.body, response);
+	response.headers.append('Content-Security-Policy', "default-src 'self'");
+	response.headers.append('Permissions-Policy', 'interest-cohort=()');
+	response.headers.append('X-Content-Type-Options', 'nosniff');
+	response.headers.append('X-Frame-Options', 'DENY');
+	response.headers.append('X-XSS-Protection', '1; mode=block');
+	response.headers.append('Referrer-Policy', 'strict-origin-when-cross-origin');
+	return response;
+}
