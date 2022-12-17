cc85d60ba1be025be7dfbdb8009a0516bbca26a8
memiux <me@memiux.com>
2022-12-16 18:09:43 -0600

    Add open graph image

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ async function handleRequest(request) {
 	response = new Response(response.body, response);
 	response.headers.append('Content-Security-Policy', "default-src 'self'");
 	response.headers.append('Permissions-Policy', 'interest-cohort=()');
-	response.headers.append('X-Content-Type-Options', 'nosniff');
 	response.headers.append('X-Frame-Options', 'DENY');
 	response.headers.append('X-XSS-Protection', '0');
+	// response.headers.append('X-Content-Type-Options', 'nosniff');
 	// response.headers.append('Referrer-Policy', 'strict-origin-when-cross-origin');
 	return response;
 }
